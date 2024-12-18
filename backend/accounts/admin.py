7e811d5fd1503ca61c81650781a5ed5b5d6b00a4
Carlos Castro <castro74carlos@gmail.com>
2024-12-17 22:10:45 -0700

    Add Category and Transaction Model

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import Account
+from .models import Account, Category, Transaction
 
 class AccountAdmin(admin.ModelAdmin):
     # ...
     list_display = ["id", "name", "account_type", "last_updated", "latest_transaction_id"]
 
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["id", "description", "date", "amount", "type", "account", "category"]
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Category, CategoryAdmin)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,14 @@
-from datetime import datetime
 from django.db import models
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Account(models.Model):
     class AccountHolder(models.TextChoices):
@@ -12,10 +21,6 @@ class Account(models.Model):
         CASH = 'C', 'Cash'
         DEBT = 'D', 'Debt'
         SAVINGS = 'S', 'Savings'
-
-
-    # id is auto, only change if expecting id numbers to be large, see below:
-    # id = models.BigAutoField(primary_key=True)  # Explicit primary key
     
     name = models.CharField(max_length=50)
     account_holder = models.CharField(
@@ -30,12 +35,36 @@ class Account(models.Model):
         max_length=2,
         choices=AccountType,
     )
-    created = models.DateTimeField(default=datetime.now())
-    last_updated = models.DateTimeField(auto_now=True)
-    latest_transaction_id = models.BigIntegerField(default=0)
+    latest_transaction_id = models.BigIntegerField(null=True, blank=True)
     mortgage = models.BooleanField(default=False)
-    # interest_rate
+    # interest_rate 
     # promo_period
+    created = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
     
 
+class Transaction(models.Model):
+    class TransactionType(models.TextChoices):
+        INCREASE = 'INC', "Increase"
+        DECREASE = 'DEC', "Decrease"
 
+    id = models.BigAutoField(primary_key=True)
+    description = models.CharField(max_length=50)
+    date = models.DateField(null=True, blank=True)
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    type = models.CharField(
+        max_length=3,
+        choices=TransactionType
+    )
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    paid_off = models.BooleanField(default=False)
+    created = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(auto_now=True)
