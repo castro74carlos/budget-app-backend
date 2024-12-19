201d232b8a4befc6fa9be4c1feea97a2feb4c3c8
Carlos Castro <castro74carlos@gmail.com>
2024-12-19 01:59:46 -0700

    Fixed By adding Users before creating migrations for other models

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import *
 
-# class AccountAdmin(admin.ModelAdmin):
-#     # ...
-#     list_display = ["id", "name", "account_type", "last_updated", "latest_transaction_id"]
+class AccountAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ["id", "name", "account_type", "last_updated", "latest_transaction_id"]
 
-# class TransactionAdmin(admin.ModelAdmin):
-#     list_display = ["id", "vendor", "date", "amount", "type", "account", "category", "paid_off"]
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["id", "vendor", "date", "amount", "type", "account", "category", "paid_off"]
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ["id", "name"]
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
 
-# class VendorAdmin(admin.ModelAdmin):
-#     list_display = ["id", "name"]
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
 
-# admin.site.register(Account, AccountAdmin)
-# admin.site.register(Transaction, TransactionAdmin)
-# admin.site.register(Category, CategoryAdmin)
-# admin.site.register(Vendor, VendorAdmin)
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Vendor, VendorAdmin)
 admin.site.register(User, UserAdmin)
