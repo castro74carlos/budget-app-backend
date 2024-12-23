15d5f56876bd3c4e745b062f8564de7cadd89f24
Carlos Castro <castro74carlos@gmail.com>
2024-12-23 03:29:17 -0700

    add tests for user API

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -7,10 +7,10 @@ from .models import User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['id', 'url', 'username', 'email', 'groups']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
-        fields = ['url', 'name']
+        fields = ['id', 'url', 'name']
